4aee1c6f996fe2f08170d413ee776141e64e6291
saifulire <2001025@iot.bdu.ac.bd>
2024-11-11 01:08:31 +0600

    Create data_type.js

---
+++ javascript/data_type.js
@@ -0,0 +1,15 @@
+const accountId = 24245678
+let accountEmail ="saifulire@repl.it"
+var accountPassword ="9382"//var does not used, cause block scopes and functional scope
+accountMaintainerCity="NYC"
+let x;
+var y;
+z=12;
+const k=4;
+//
+//accountId= 55553434 //accountId can not be able to read here cause of const keyword variable
+accountEmail= "shark@microsoft.com"
+accountPassword= "9393"
+accountMaintainerCity="astana_dhaka" 
+
+console.log([accountEmail,accountId,accountPassword,accountMaintainerCity,x,y,z,k]);
