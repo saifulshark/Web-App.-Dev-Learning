e4f8850ff94a5eb2cdfc0f25121687a32f3c3f87
saifulire <2001025@iot.bdu.ac.bd>
2024-11-14 02:04:06 +0600

    added
    js starting literals here on type conversion and data types revision

---
+++ javascript/datatypes_ECMA.js
@@ -0,0 +1,9 @@
+"use strict" //treat all js code as newer version of js
+// let a=window.alert("caution");
+//two types of data types here: primitive and non-premitive/derived from premitives
+//primitves are: string,number=(int,bigint,float,char,double),boolean,null,undefined,symbol
+//N.B.: datatype of null=object*** and undefined=undefined
+//non-primitives are: object
+console.log(typeof(null));
+console.log(typeof(undefined));
+
---
+++ javascript/type_conversion.js
@@ -0,0 +1,38 @@
+// let score="33adf";
+// console.log(typeof score)
+// console.log(typeof(score));
+// let valinnum =Number(score);
+// console.log(typeof valinnum);
+// console.log(valinnum);
+
+// let score1=null;
+// console.log(typeof score1)
+// console.log(typeof(score1));
+// let valinnum1 =Number(score1);
+// console.log(typeof valinnum1);
+// console.log(valinnum1);
+
+// let score12=undefined;
+// console.log(typeof score12)
+// console.log(typeof(score12));
+// let valinnum12 =Number(score12);
+// console.log(typeof valinnum12);
+// console.log(valinnum12);
+
+// let score123=true;
+// console.log(typeof (true));
+// let v=Number(score123);
+// console.log(typeof v);
+// console.log(v);
+//number conversion here: 
+// "33"=>33; "23aa"=>NaN; ture=>1 false=>0;
+
+let isLoggedIn = "";
+let boolisLoggedIn= Boolean(isLoggedIn);
+console.log(boolisLoggedIn);
+console.log(1+3+"1");
+console.log("1"-1);
+console.log(2-"3");
+console.log(+true)
+console.log(+"")
+
