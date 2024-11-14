8bd6dbd4396813d5c363a66a96526386e29ce37d
saifulire <2001025@iot.bdu.ac.bd>
2024-11-14 21:24:34 +0600

    added
    endded of stack and heap memory

---
+++ javascript/stack_heap.js
@@ -0,0 +1,13 @@
+let value="xChain";
+value="chainX";
+console.log(value);
+let user1={
+    email: "saifulire@hotmail.com",
+    bkash_id_no: "23289"
+}
+let user2=user1;
+user2.email="2001025@iot.bdu.ac.bd";
+user2.bkash_id_no= "ax2387"
+console.log(user1.email,user1.bkash_id_no);
+console.log(user2.email,user2.bkash_id_no);
+
