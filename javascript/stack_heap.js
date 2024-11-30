2243366f052c3af1b9a5a82be8d031529502f39c
saifulire <2001025@iot.bdu.ac.bd>
2024-12-01 04:03:35 +0600

    added
    function and heap reivse

--- javascript/stack_heap.js
+++ javascript/stack_heap.js
@@ -11,3 +11,12 @@ user2.bkash_id_no= "ax2387"
 console.log(user1.email,user1.bkash_id_no);
 console.log(user2.email,user2.bkash_id_no);
 
+//Memory two types: Stack Memory and Heap Memory
+
+//primitive:symbol,string,number,boolean,null,undefined,BigInt
+//Non-primitive:objects,array,function
+
+
+//Stack(primitive: makes a copy of variable to a container, when value gets changed,,only copy gets changed and main value remains unchanged)
+//Heap(non-primitive: makes a reference of the variable values, when variable value gets changed,,main value also gets changed)
+
