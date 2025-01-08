02dd488970dc0b8a4710785380b3591164a9bf68
Saiful Islam <136887493+saifulshark@users.noreply.github.com>
2025-01-08 17:40:40 +0600

    Create loopingAdv_1.js

---
+++ javascript/loopingAdv.js
@@ -0,0 +1,81 @@
+// console.log("hellow,world");
+// const greetings="goodbye the world";
+// for(const greet of greetings){
+//   if(greet===" "){continue}
+//   console.log(greet);
+//   console.log(`Each of char is ${greet}`);
+// }
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+// let myArray = ['flash', "batman", "superman"]
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+// const arr = [1, 2, 3, 4, 5]
+// const arr1=[
+//   {
+//     "color": "purple",
+//     "type": "minivan",
+//     "registration": new Date('2017-01-03'),
+//     "capacity": 7
+//   },
+//   {
+//     "color": "red",
+//     "type": "station wagon",
+//     "registration": new Date('2018-03-03'),
+//     "capacity": 5
+//   }]
+// for (const num of arr1) {
+//     console.log(num);
+// }
+
+// const greetings = "Hello world!"
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`);
+// }
+
+// Maps
+//objects set up is work on basis of key uniqueness only not watching values
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "Indiana")
+// map.set('IN', "India")
+// map.set('US(IN)', "India")
+// map.set('IN(US)', "India")
+
+// map.set('BN', "Bangladesh")
+
+
+// console.log(map)
+// for(const result of map)
+//   {
+//     console.log(result);
+//   }
+
+// for (const [key, value] of map) {
+// console.log(key, ':-', value);
+// }
+//it not will be run as only of object, if it turn into an array or array with object then will run;
+// for (const [key, value] of map.entries())
+// const myObject = {
+//                      game1: 'NFS',
+//                      game2: 'Spiderman'
+//                  }
+
+// for (const item of myObject) {
+//     console.log(item);
+
+// }
