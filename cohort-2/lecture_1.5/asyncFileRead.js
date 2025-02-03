2ac2c1803c777e256e45704bcabdfeb705f2b42e
Saiful Islam <2001025@iot.bdu.ac.bd>
2025-02-04 00:11:42 +0600

    Update asyncFileRead.js
    added asyc example

--- cohort-2/lecture_1.5/asyncFileRead.js
+++ cohort-2/lecture_1.5/asyncFileRead.js
@@ -2,3 +2,7 @@ const fs = require('fs');
 fs.readFile('a.txt', 'utf8', (err, data) => (
     console.log('File content:', `${data}`)
 ));
+setTimeout(() => {
+    console.log('After 20 seconds');
+}, 20000);
+console.log('Reading file...');
